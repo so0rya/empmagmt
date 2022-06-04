6869f4aa386be7dfe1198392a5128e5bf361f464
Soorya <ssooryan@gmail.com>
2022-06-04 10:16:53 +0530

    5viewsadded

--- calculator/urls.py
+++ calculator/urls.py
@@ -5,4 +5,8 @@ from calculator import views
 urlpatterns=[
     path("home",views.HomeView.as_view()),
     path("add",views.AddView.as_view()),
+    path("sub", views.SubView.as_view()),
+    path("div", views.DivView.as_view()),
+    path("fact",views.FactView.as_view())
+
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.shortcuts import render
 from django.views.generic import View
 
@@ -15,4 +17,31 @@ class AddView(View):
         # print(result)
         return render(request, "add.html",{"res":result})
 
+class SubView(View):
+    def get(self,request):
+        return render(request,"sub.html")
+    def post(self,request):
+        n1=request.POST.get("num1")
+        n2=request.POST.get("num2")
+        result=int(n1)-int(n2)
+        # print(result)
+        return render(request, "sub.html",{"res":result})
 
+class DivView(View):
+    def get(self,request):
+        return render(request,"div.html")
+    def post(self,request):
+        n1=request.POST.get("num1")
+        n2=request.POST.get("num2")
+        result=int(n1)/int(n2)
+        # print(result)
+        return render(request, "div.html",{"res":result})
+
+class FactView(View):
+    def get(self,request):
+        return render(request,"fact.html")
+    def post(self,request):
+        n1=request.POST.get("num1")
+        result=math.factorial(int(n1))
+        # print(result)
+        return render(request, "fact.html",{"res":result})
